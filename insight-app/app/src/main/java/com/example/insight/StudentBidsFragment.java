b3c0739cbc88ac38f90a808a99a7cc3c0474e243
Alfons Fernaldy <alfonsfernaldy42@gmail.com>
2021-04-24 17:12:29 +0700

    Added project dependencies and fragments

---
+++ insight-app/app/src/main/java/com/example/insight/StudentBidsFragment.java
@@ -0,0 +1,28 @@
+package com.example.insight;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StudentBidsFragment extends Fragment {
+
+    public StudentBidsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_bids, container, false);
+    }
+}
